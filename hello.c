c7d187e048150a1d669281b071bfe185ecae2370
javaGhost1 <dennismurage97@gmail.com>
2025-02-10 22:59:51 +0300

    Day one of C programming

---
+++ hello.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main () {
+	char name[50];
+	int number;
+	
+	printf("Hello player \n");
+	printf("what's your name? ");
+	scanf("%s", name); // get string input
+
+	printf("let's play a Game\n");
+	printf("Pick a number between 1 and 9 ? ");
+	scanf(" %d", &number); // get integer input
+	printf("%s have entered the number Nine: %d\n", name, number);
+	
+	while (number > 0 and number < 9){
+		if (number <= 0) {
+			printf("Too low\n");
+		} else if (number < 9) {
+			printf("Too high\n");
+		}else{
+			int square;
+			square = number*number; //find the square of input
+			printf("It's square is %d\n", square);
+		}
+	}
+	return 0;
+}
